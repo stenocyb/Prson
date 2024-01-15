c50ca8138986ee191c25bd7a0b443275fa40a8d3
stenocyb <luxfuxnux@gmail.com>
2024-01-15 10:55:29 +0100

    Added --browser option for the user to choose a specific browser website lookups

--- prson.py
+++ prson.py
@@ -24,45 +24,45 @@ websites_file = os.getcwd() + os.path.sep + 'websites.txt'
 
 ask_questions = False
 
-## Functions
+# Functions
 
-# Checks if a string is an email or not, using regex
+## Checks if a string is an email or not, using regex
 def is_email(email: str) -> bool:
     if(re.fullmatch(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b', email)):
         return True
     else:
         return False
-# Checks whether a string is a phone number or not, using regex 
+## Checks whether a string is a phone number or not, using regex 
 def is_phone_number(phone_number: str) -> bool:
     pattern = re.compile('^[\dA-Z]{3}-[\dA-Z]{3}-[\dA-Z]{4}$', re.IGNORECASE)
-    return pattern.match(phone_number) is not None
+    return pattern.match(phone_number) is not None or phone_number.replace('+', '').isnumeric()
  
-# Displays a conclusive screen
+## Displays a conclusive screen
 def display_wrapup(start_time: float):
     print('- Execution time: %s seconds' % (time.time() - start_time))
     print('- Information has been saved to: ' + output_directory)
 
-# Executes a list of commands stored in a file
+## Executes a list of commands stored in a file
 def execute_commands(input_variable: str, commands_file: str, type: str):
     content = read_commands_file(commands_file, type)
 
-    # Executes commands, replacing $INPUT with argv[1] and $OUTPUT_DIR with output_directory variable
+    # Executes commands, replacing $INPUT with 'input' argument (email/phone/username) and $OUTPUT_DIR with output_directory variable
     for command in content:
         print(os.popen(str(command).replace('$INPUT', input_variable).replace('$OUTPUT_DIR', output_directory)).read())
 
-# Fills out a form on a specific website using Firefox browser
-def fill_out_form(link: str, field_attribute: str, field_value: str, field_input: str, driver: webdriver.Firefox):
+## Fills out a form on a specific website using Firefox browser
+def fill_out_form(link: str, field_attribute: str, field_value: str, field_input: str, driver):
     driver.get(url=link)
     form = driver.find_element(field_attribute, field_value)
     form.send_keys(field_input)
     form.submit()
 
-# Opens a new browser tab
-def open_new_tab(driver: webdriver.Firefox):
+## Opens a new browser tab
+def open_new_tab(driver):
     driver.execute_script('window.open('');')
     driver.switch_to.window(driver.window_handles[-1])
 
-# Asks for an answer from a user concerning a yes-no question
+## Asks for an answer from a user concerning a yes-no question
 def ask_polar_question(question: str) -> bool:
     answer = input(question + ' [Y/n]')
 
@@ -71,7 +71,7 @@ def ask_polar_question(question: str) -> bool:
     else:
         return True
 
-# Reads the commands file in YAML format, returns the contained commands sorted by type in a list
+## Reads the commands file in YAML format, returns the contained commands sorted by type in a list
 def read_commands_file(commands_file: str, type: str) -> list:
     commands = []
 
@@ -91,13 +91,20 @@ def read_commands_file(commands_file: str, type: str) -> list:
         print('Error: Commands file ' + commands_file + ' not found.')
         exit(1)
 
-# Reads the websites file in YAML format, returns its websites sorted by type in a list
-def read_websites_file_and_visit(websites_file: str, type: str, input_str: str) -> list:
+## Reads the websites file in YAML format, returns its websites sorted by type in a list
+def read_websites_file_and_visit(websites_file: str, type: str, input_str: str, web_driver: str) -> list:
     try:
         with open(websites_file, 'r') as file:
             try:
                 yaml_data = yaml.safe_load(file)
-                driver = webdriver.Firefox()
+
+                if web_driver == 'firefox':
+                    driver = webdriver.Firefox()
+                elif web_driver == 'chrome':
+                    driver = webdriver.Chrome()
+                elif web_driver == 'edge':
+                    driver = webdriver.Edge()
+
                 iteration_counter = 0
                 website_counter = 0
 
@@ -134,8 +141,8 @@ def read_websites_file_and_visit(websites_file: str, type: str, input_str: str) 
         exit(1)
 
 
-# Looks up an email address
-def lookup_email(email: str, only_commands: bool, only_websites: bool):
+## Looks up an email address
+def lookup_email(email: str, only_commands: bool, only_websites: bool, browser: str):
     if ask_questions:
         if not only_websites:
             if ask_polar_question('Do you want to execute system commands in order to perform an OSINT lookup of the provided email address?'):
@@ -143,11 +150,11 @@ def lookup_email(email: str, only_commands: bool, only_websites: bool):
 
                 if not only_commands:
                     if ask_polar_question('Do you want to start Firefox in order to perform online information gathering?'):
-                        read_websites_file_and_visit(websites_file, 'email', email)
+                        read_websites_file_and_visit(websites_file, 'email', email, browser)
         else:
             if not only_commands:
                 if ask_polar_question('Do you want to start Firefox in order to perform online information gathering?'):
-                    read_websites_file_and_visit(websites_file, 'email', email)
+                    read_websites_file_and_visit(websites_file, 'email', email, browser)
             else:
                 print('Error: Conflicting command line options, -w and -c can not be used together.')
                 exit(1)
@@ -155,16 +162,16 @@ def lookup_email(email: str, only_commands: bool, only_websites: bool):
         if only_commands:
             execute_commands(email, commands_file, 'email')
         elif only_websites:
-            read_websites_file_and_visit(websites_file, 'email', email)
+            read_websites_file_and_visit(websites_file, 'email', email, browser)
         elif not only_commands and not only_websites:
             execute_commands(email, commands_file, 'email')
-            read_websites_file_and_visit(websites_file, 'email', email)
+            read_websites_file_and_visit(websites_file, 'email', email, browser)
         else:
             print('Error: Conflicting command line options, -w and -c can not be used together.')
             exit(1)
 
-# Looks up a telephone number
-def lookup_phone_number(number: str, only_commands: bool, only_websites: bool):
+## Looks up a telephone number
+def lookup_phone_number(number: str, only_commands: bool, only_websites: bool, browser: str):
     if ask_questions:
         if not only_websites:
             if ask_polar_question('Do you want to execute system commands in order to perform an OSINT lookup of the provided telephone number?'):
@@ -172,11 +179,11 @@ def lookup_phone_number(number: str, only_commands: bool, only_websites: bool):
 
                 if not only_commands:
                     if ask_polar_question('Do you want to start Firefox in order to perform online information gathering?'):
-                        read_websites_file_and_visit(websites_file, 'phone', number)
+                        read_websites_file_and_visit(websites_file, 'phone', number, browser)
         else:
             if not only_commands:
                 if ask_polar_question('Do you want to start Firefox in order to perform online information gathering?'):
-                    read_websites_file_and_visit(websites_file, 'phone', number)
+                    read_websites_file_and_visit(websites_file, 'phone', number, browser)
             else:
                 print('Error: Conflicting command line options, -w and -c can not be used together.')
                 exit(1)
@@ -184,16 +191,16 @@ def lookup_phone_number(number: str, only_commands: bool, only_websites: bool):
         if only_commands:
             execute_commands(number, commands_file, 'phone')
         elif only_websites:
-            read_websites_file_and_visit(websites_file, 'phone', number)
+            read_websites_file_and_visit(websites_file, 'phone', number, browser)
         elif not only_commands and not only_websites:
             execute_commands(number, commands_file, 'phone')
-            read_websites_file_and_visit(websites_file, 'phone', number)
+            read_websites_file_and_visit(websites_file, 'phone', number, browser)
         else:
             print('Error: Conflicting command line options, -w and -c can not be used together.')
             exit(1)
 
-# Looks up a username
-def lookup_username(username: str, only_commands: bool, only_websites: bool):
+## Looks up a username
+def lookup_username(username: str, only_commands: bool, only_websites: bool, browser: str):
     if ask_questions:
         if not only_websites:
             if ask_polar_question('Do you want to execute system commands in order to perform an OSINT lookup of the provided username?'):
@@ -201,11 +208,11 @@ def lookup_username(username: str, only_commands: bool, only_websites: bool):
                 
                 if not only_commands:
                     if ask_polar_question('Do you want to start Firefox in order to perform online information gathering?'):
-                        read_websites_file_and_visit(websites_file, 'username', username)
+                        read_websites_file_and_visit(websites_file, 'username', username, browser)
         else:
             if not only_commands:
                 if ask_polar_question('Do you want to start Firefox in order to perform online information gathering?'):
-                        read_websites_file_and_visit(websites_file, 'username', username)
+                        read_websites_file_and_visit(websites_file, 'username', username, browser)
             else:
                 print('Error: Conflicting command line options, -w and -c can not be used together.')
                 exit(1)
@@ -213,10 +220,10 @@ def lookup_username(username: str, only_commands: bool, only_websites: bool):
         if only_commands:
             execute_commands(username, commands_file, 'username')
         elif only_websites:
-            read_websites_file_and_visit(websites_file, 'username', username)
+            read_websites_file_and_visit(websites_file, 'username', username, browser)
         elif not only_commands and not only_websites:
             execute_commands(username, commands_file, 'username')
-            read_websites_file_and_visit(websites_file, 'username', username)
+            read_websites_file_and_visit(websites_file, 'username', username, browser)
         else:
             print('Error: Conflicting command line options, -w and -c can not be used together.')
             exit(1)
@@ -236,7 +243,8 @@ def main():
         parser.add_argument('-o', '--output', help='Specify an output directory') 
         parser.add_argument('-cf', '--commands-file', help='Specify a commands file')
         parser.add_argument('-wf', '--websites-file', help='Specify a websites file')
-        parser.add_argument('-e', '--email', action='store_true', help='Force input to be handled as an email address') # Note: action='store_true makes the switch optional'
+        parser.add_argument('-b', '--browser', help='Indicate the browser to use for a website lookup (firefox, chrome, edge)')
+        parser.add_argument('-e', '--email', action='store_true', help='Force input to be handled as an email address') # Note: action='store_true' makes the switch optional
         parser.add_argument('-n', '--number', action='store_true', help='Force input string to be processed as a telephone number')
         parser.add_argument('-u', '--username', action='store_true', help='Force input string to be treated as username')
         parser.add_argument('-q', '--question', action='store_true', help='Asks questions before doing any major task')
@@ -266,22 +274,28 @@ def main():
         if args.question:
             ask_questions = True
 
+        # Check which browser to use for website lookups
+        if args.browser:
+            browser = args.browser
+        else:
+            browser = "firefox"
+
         # Evaluate whether input string should be forced to be treated as certain type
         if args.email or args.number or args.username:
             if args.email:
-                lookup_email(args.input, args.commands, args.websites)
+                lookup_email(args.input, args.commands, args.websites, browser)
             elif args.number:
-                lookup_phone_number(args.input, args.commands, args.websites)
+                lookup_phone_number(args.input, args.commands, args.websites, browser)
             else:
-                lookup_username(args.input, args.commands, args.websites)
+                lookup_username(args.input, args.commands, args.websites, browser)
         # Evaluate automatically type of input string and act accordingly
         else: 
             if is_email(args.input):
-                lookup_email(args.input, args.commands, args.websites)
-            elif is_phone_number(args.input) or args.input.replace('+', '').isnumeric():
-                lookup_phone_number(args.input, args.commands, args.websites)
+                lookup_email(args.input, args.commands, args.websites, browser)
+            elif is_phone_number(args.input):
+                lookup_phone_number(args.input, args.commands, args.websites, browser)
             else:
-                lookup_username(args.input, args.commands, args.websites)
+                lookup_username(args.input, args.commands, args.websites, browser)
 
         # Display information wrapup
         display_wrapup(start_time)
